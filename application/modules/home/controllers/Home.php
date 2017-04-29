cef973cc288113f61b676317ba27154c4bab4e8e
cake <s5640216@gmail.com>
2017-04-29 14:50:26 +0800

    Initialize CI environment

---
+++ application/modules/home/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends MX_Controller {
+
+	
+	public function index()
+	{
+		echo "目前啟動的路徑為 : software\application\modules\home\controllers\Home.php";
+	}
+}
